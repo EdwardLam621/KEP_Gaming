a627b093c8aed550a29f230620cbdec598bd2a35
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-07 10:49:59 -0800

    Update default window size in UWP app.xaml.cs

--- Game/Game.UWP/App.xaml.cs
+++ Game/Game.UWP/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -70,6 +71,10 @@ namespace Game.UWP
                 // parameter
                 rootFrame.Navigate(typeof(MainPage), e.Arguments);
             }
+
+            ApplicationView.PreferredLaunchViewSize = new Size(720, 1440);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
             // Ensure the current window is active
             Window.Current.Activate();
         }
