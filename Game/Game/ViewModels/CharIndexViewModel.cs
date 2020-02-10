b871ebd6b84e60f2723498a9dd58c1ec3c77be29
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-09 21:34:10 -0800

    add SortDataset method to CharIndexViewModel

--- Game/Game/ViewModels/CharIndexViewModel.cs
+++ Game/Game/ViewModels/CharIndexViewModel.cs
@@ -127,5 +127,22 @@ namespace Game.ViewModels
 
         #endregion DataOperations_CRUDi
 
+        #region SortDataSet
+
+        /// <summary>
+        /// The Sort Order for the CharacterModel
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <returns></returns>
+        public override List<CharacterModel> SortDataset(List<CharacterModel> dataset)
+        {
+            return dataset
+                    .OrderBy(a => a.Name)
+                    .ThenBy(a => a.Description)
+                    .ToList();
+        }
+
+        #endregion SortDataSet
+
     }
 }
