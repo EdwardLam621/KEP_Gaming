adf6d85c76bb772ea714b93f4191c8418ce31c5f
Edward <lamhoutenged@seattleu.edu>
2020-02-09 21:42:48 -0800

    Add readonly GenericViewModel<CharacterModel> ViewModel in CharacterReadPage

--- Game/Game/Views/Character/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Character/CharacterReadPage.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.ComponentModel;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Game.ViewModels;
+using System;
+using Game.Models;
 
 namespace Game.Views.Character
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterReadPage : ContentView
     {
+        readonly GenericViewModel<CharacterModel> ViewModel;
+
         public CharacterReadPage()
         {
             InitializeComponent();
