0dc8a82340cb291d001920709fa6a6ab4abbf044
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-09 21:06:37 -0800

    Add singleton region to CharIndexViewModel

--- Game/Game/ViewModels/CharIndexViewModel.cs
+++ Game/Game/ViewModels/CharIndexViewModel.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Game.Models;
+using Game.Views;
+using System;
+using Xamarin.Forms;
+using System.Linq;
 using System.Collections.Generic;
-using System.Text;
+using Game.Services;
 
 namespace Game.ViewModels
 {
-    class CharIndexViewModel
+
+    public class CharIndexViewModel : BaseViewModel<CharacterModel>
     {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile CharIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static CharIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new CharIndexViewModel();
+                            instance.Initialize();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
     }
 }
