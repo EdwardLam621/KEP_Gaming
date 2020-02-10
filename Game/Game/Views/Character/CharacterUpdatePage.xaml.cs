4dbad96add90cf78cd286b73f00092d1ffe041c8
Edward <lamhoutenged@seattleu.edu>
2020-02-09 22:09:38 -0800

    Added Save_Clicked and Cancel_Clicked method in CharacterUpdatePage, related toolbar button in xaml

--- Game/Game/Views/Character/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterUpdatePage.xaml.cs
@@ -27,6 +27,32 @@ namespace Game.Views
 
         }
 
-       
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            // If the image in the data box is empty, use the default one..
+            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
+            {
+                ViewModel.Data.ImageURI = Services.ItemService.DefaultImageURI;
+            }
+
+            MessagingCenter.Send(this, "Update", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel and close this page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
     }
 }
