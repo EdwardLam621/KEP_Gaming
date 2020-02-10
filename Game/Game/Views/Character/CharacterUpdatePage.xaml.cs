418941194ed3bb1afb927cffc4ba9f89bab5cec2
Edward <lamhoutenged@seattleu.edu>
2020-02-09 22:08:17 -0800

    Updated CharacterUpdatePage constructor and added related pickerbox in xaml

--- Game/Game/Views/Character/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterUpdatePage.xaml.cs
@@ -6,14 +6,27 @@ using Game.Models;
 
 namespace Game.Views
 {
-    public partial class CharacterUpdatePage : ContentView
+    public partial class CharacterUpdatePage : ContentPage
     {
-
+        // View Model for Item
         readonly GenericViewModel<CharacterModel> ViewModel;
 
-        public CharacterUpdatePage()
+        /// <summary>
+        /// Constructor that takes and existing data item
+        /// </summary>
+        public CharacterUpdatePage(GenericViewModel<CharacterModel> data)
         {
             InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Update " + data.Title;
+
+            //Need to make the SelectedItem a string, so it can select the correct item.
+            SkillPicker.SelectedItem = data.Data.Skill.ToString();
+
         }
+
+       
     }
 }
