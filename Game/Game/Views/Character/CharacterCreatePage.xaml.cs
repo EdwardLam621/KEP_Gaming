f3df79f060136a0dcea7a2560223e34e8e65892f
Ka <ka@Kas-MacBook-Air.hsd1.wa.comcast.net>
2020-02-11 00:38:45 -0800

    add Health stepper to Character Create page with min and max value

--- Game/Game/Views/Character/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterCreatePage.xaml.cs
@@ -43,13 +43,23 @@ namespace Game.Views
         }
 
         /// <summary>
-        /// Catch the change to the Stepper for Range
+        /// Catch the change to the Stepper for Level
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         void Level_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         {
             LevelValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        /// <summary>
+        /// Catch the change to the Stepper for Health
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Health_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            HealthValue.Text = String.Format("{0}", e.NewValue);
         }
     }
 
