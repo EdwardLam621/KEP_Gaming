0eef393b98786fedde3142dff0293b91133a2410
Ka <ka@Kas-MacBook-Air.hsd1.wa.comcast.net>
2020-02-11 08:28:58 -0800

    Changed ItemCreate to trap the Android Back Button

--- Game/Game/Views/Character/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterCreatePage.xaml.cs
@@ -91,6 +91,11 @@ namespace Game.Views
         {
             OffenseValue.Text = String.Format("{0}", e.NewValue);
         }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
     }
 
 }
