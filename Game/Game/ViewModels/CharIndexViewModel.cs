492d1fc98c1cb2e9086d56c4bc8400c4881864fb
Ka <ka@Kas-MacBook-Air.hsd1.wa.comcast.net>
2020-02-10 22:47:58 -0800

    load default characters into Character Index Page

--- Game/Game/ViewModels/CharIndexViewModel.cs
+++ Game/Game/ViewModels/CharIndexViewModel.cs
@@ -116,10 +116,10 @@ namespace Game.ViewModels
             return myList;
         }
 
-        /// <summary>
-        /// Load the Default Data
-        /// </summary>
-        /// <returns></returns>
+        ///// <summary>
+        ///// Load the Default Data
+        ///// </summary>
+        ///// <returns></returns>
         public override List<CharacterModel> GetDefaultData()
         {
             return DefaultData.LoadData(new CharacterModel());
--- Game/Game/Views/Character/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Character/CharacterIndexPage.xaml.cs
@@ -28,6 +28,60 @@ namespace Game.Views
             BindingContext = ViewModel = CharIndexViewModel.Instance;
         }
 
-        
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        //async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        //{
+            /*
+            CharacterModel data = args.SelectedItem as CharacterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(data)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+            */
+        //}
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ItemCreatePage(new GenericViewModel<ItemModel>())));
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
     }
 }
