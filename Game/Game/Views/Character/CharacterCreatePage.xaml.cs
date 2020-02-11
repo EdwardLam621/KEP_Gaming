c1b9adad7c7ae4da7e1497e0db0d69893d050fda
Ka <ka@Kas-MacBook-Air.hsd1.wa.comcast.net>
2020-02-11 00:44:58 -0800

    add Defense Atribbute stepper to Character Create page with min and max value

--- Game/Game/Views/Character/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterCreatePage.xaml.cs
@@ -71,6 +71,16 @@ namespace Game.Views
         {
             SpeedValue.Text = String.Format("{0}", e.NewValue);
         }
+
+        /// <summary>
+        /// Catch the change to the Stepper for Defense
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Defense_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            DefenseValue.Text = String.Format("{0}", e.NewValue);
+        }
     }
 
 }
