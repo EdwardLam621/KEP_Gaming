25f46aeecca3fec3e390e1dd43098735123b8944
Edward <lamhoutenged@seattleu.edu>
2020-02-12 14:40:12 -0800

    Updated using namespaces of MonsterIndexViewModel

--- Game/Game/ViewModels/MonsterIndexViewModel.cs
+++ Game/Game/ViewModels/MonsterIndexViewModel.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Game.Models;
+using Game.Views;
+using System;
+using Xamarin.Forms;
+using System.Linq;
 using System.Collections.Generic;
-using System.Text;
+using Game.Services;
 
 namespace Game.ViewModels
 {
     class MonsterIndexViewModel
     {
+
     }
 }
