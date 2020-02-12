30699a0ba1a50feb10c678c8f82f159ea0f5c49a
Edward <lamhoutenged@seattleu.edu>
2020-02-12 14:25:41 -0800

    Created and Implemented MonsterModel inherited by CreatureModel

---
+++ Game/Game/Models/MonsterModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    class MonsterModel : CreatureModel<MonsterModel>
+    {
+        public List<ItemModel> DropItems;
+
+        public MonsterModel()
+        {
+            this.Name = "this is Name";
+            this.Description = "this is Monster Description";
+            this.CurrentHealth = MaxHealth;
+        }
+
+        public int Level { get; set; }
+
+        public override void Update(MonsterModel newData)
+        {
+            if (newData == null)
+            {
+                return;
+            }
+
+            // Update all the fields in the Data, except for the Id and guid
+            Name = newData.Name;
+            Level = newData.Level;
+            DropItems = newData.DropItems;
+            MaxHealth = newData.MaxHealth;
+            CurrentHealth = newData.CurrentHealth;
+            Description = newData.Description;
+            SpeedAttribute = newData.SpeedAttribute;
+            OffenseAttribute = newData.OffenseAttribute;
+            DefenseAttribute = newData.DefenseAttribute;
+            ImageURI = newData.ImageURI;
+            Skill = newData.Skill;
+        }
+    }
+}
