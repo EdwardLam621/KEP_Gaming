f3a57fdfd33e69701bb2a1cff0d2219257d80319
Edward <lamhoutenged@seattleu.edu>
2020-02-12 15:06:18 -0800

    Updated MonsterIndexPage.xaml.cs

--- Game/Game/Views/Monsters/MonsterIndexPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterIndexPage.xaml.cs
@@ -1,20 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.ComponentModel;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Game.Models;
+using Game.ViewModels;
 
 namespace Game.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MonsterIndexPage : ContentPage
     {
+
+        // The view model, used for data binding
+        readonly MonsterIndexViewModel ViewModel;
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the ItemIndexView Model
+        /// </summary>
         public MonsterIndexPage()
         {
             InitializeComponent();
+
+            BindingContext = ViewModel = MonsterIndexViewModel.Instance;
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+
+            CharacterModel data = args.SelectedItem as CharacterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(data)));
+
+            // Manually deselect item.
+            MonsterListView.SelectedItem = null;
+
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void AddCharacter_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new MonsterCreatePage(new GenericViewModel<MonsterModel>())));
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
         }
     }
 }
