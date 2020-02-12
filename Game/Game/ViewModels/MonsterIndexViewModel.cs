24d1fec295d031a755bae9fd3b9aee6abcda773a
Edward <lamhoutenged@seattleu.edu>
2020-02-12 14:43:15 -0800

    Implemented MonsterIndexViewModel

--- Game/Game/ViewModels/MonsterIndexViewModel.cs
+++ Game/Game/ViewModels/MonsterIndexViewModel.cs
@@ -8,8 +8,141 @@ using Game.Services;
 
 namespace Game.ViewModels
 {
-    class MonsterIndexViewModel
+    class MonsterIndexViewModel : BaseViewModel<MonsterIndexViewModel>
     {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile MonsterIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static MonsterIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new MonsterIndexViewModel();
+                            instance.Initialize();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// The constructor subscribes message listeners for crudi operations
+        /// </summary>
+        public MonsterIndexViewModel()
+        {
+            Title = "Monsters";
+
+            #region Messages
+
+            // Register the Create Message
+            MessagingCenter.Subscribe<MonsterCreatePage, MonsterModel>(this, "Create", async (obj, data) =>
+            {
+                await CreateAsync(data as MonsterIndexViewModel);
+            });
+
+            // Register the Update Message
+            MessagingCenter.Subscribe<MonsterUpdatePage, MonsterModel>(this, "Update", async (obj, data) =>
+            {
+                // Have the item update itself
+                data.Update(data);
+
+                await UpdateAsync(data as MonsterModel);
+            });
+
+            // Register the Delete Message
+            MessagingCenter.Subscribe<MonsterDeletePage, MonsterModel>(this, "Delete", async (obj, data) =>
+            {
+                await DeleteAsync(data as MonsterModel);
+            });
+
+            // Register the Set Data Source Message
+            MessagingCenter.Subscribe<AboutPage, int>(this, "SetDataSource", async (obj, data) =>
+            {
+                await SetDataSource(data);
+            });
+
+            // Register the Wipe Data List Message
+            MessagingCenter.Subscribe<AboutPage, bool>(this, "WipeDataList", async (obj, data) =>
+            {
+                await WipeDataListAsync();
+            });
+
+            #endregion Messages
+
+        }
+
+        #endregion Constructor
+
+        #region DataOperations_CRUDi
+
+        /// <summary>
+        /// Returns the Monster passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public MonsterModel CheckIfItemExists(MonsterModel data)
+        {
+            // This will walk the items and find if there is one that is the same.
+            // If so, it returns the item...
+
+            var myList = Dataset.Where(a =>
+                                        a.Name == data.Name)
+                                        .FirstOrDefault();
+
+            if (myList == null)
+            {
+                // it's not a match, return false;
+                return null;
+            }
+
+            return myList;
+        }
+
+        ///// <summary>
+        ///// Load the Default Data
+        ///// </summary>
+        ///// <returns></returns>
+        public override List<MonsterModel> GetDefaultData()
+        {
+            return DefaultData.LoadData(new MonsterModel());
+        }
+
+        #endregion DataOperations_CRUDi
+
+        #region SortDataSet
+
+        /// <summary>
+        /// The Sort Order for the MonsterModel
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <returns></returns>
+        public override List<MonsterModel> SortDataset(List<MonsterModel> dataset)
+        {
+            return dataset
+                    .OrderBy(a => a.Name)
+                    .ThenBy(a => a.Description)
+                    .ToList();
+        }
+
+        #endregion SortDataSet
+
 
     }
 }
