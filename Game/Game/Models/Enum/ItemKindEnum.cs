6ec73e72275ab6856d0dca92b6c67633d6cfe6b1
Edward <lamhoutenged@seattleu.edu>
2020-02-13 09:21:29 -0800

    Added ItemKindEnum

---
+++ Game/Game/Models/Enum/ItemKindEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+    public enum ItemKindEnum
+    {
+        Armor,
+        CombatWeapon,
+        RangedWeapon
+    }
+}
