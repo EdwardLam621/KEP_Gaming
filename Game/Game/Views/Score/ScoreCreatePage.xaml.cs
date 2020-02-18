6befa9e6f048defa84aceb71c7724d9008a625f7
Edward <lamhoutenged@seattleu.edu>
2020-02-18 13:06:17 -0800

    Added Number Of Turns stepper in ScoreCreatePage

--- Game/Game/Views/Score/ScoreCreatePage.xaml.cs
+++ Game/Game/Views/Score/ScoreCreatePage.xaml.cs
@@ -65,5 +65,15 @@ namespace Game.Views
         {
             RoundValue.Text = String.Format("{0}", e.NewValue);
         }
+
+        // <summary>
+        /// Catch the change to the Stepper for TurnCount
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void TurnCount_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TurnValue.Text = String.Format("{0}", e.NewValue);
+        }
     }
 }
