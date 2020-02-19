73e706e76cb9b15e98844e1950d45408f0f27016
Ka <khanhphan0214@gmail.com>
2020-02-18 23:34:28 -0800

    added comment on baseModel class

--- Game/Game/Models/BaseModel.cs
+++ Game/Game/Models/BaseModel.cs
@@ -11,6 +11,7 @@ namespace Game.Models
         // Location to the image for the item.  Will come from the server as a fully qualified URI example:  https://developer.android.com/images/robot-tiny.png
         public string ImageURI { get; set; }
 
+        //method to update a base model 
         public virtual void Update(T newData)
         {
 
