1b5925fdbf9b028d6e0ce9d131826ff3accf7d54
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-21 09:07:43 -0800

    Add constructors to ArenaModel

--- Game/Game/Models/ArenaModel.cs
+++ Game/Game/Models/ArenaModel.cs
@@ -6,6 +6,22 @@ namespace Game.Models
 {
     public class ArenaModel
     {
+        private List<DungeonCharacterModel> Party;
+        private List<DungeonMonsterModel> Mobs;
+        private int CurrentRound = 1;
+
+        public ArenaModel()
+        {
+
+        }
+
+        public ArenaModel(List<DungeonCharacterModel> party, List<DungeonMonsterModel> mobs)
+        {
+            Party = party;
+            Mobs = mobs;
+        }
+
+
 
     }
 }
