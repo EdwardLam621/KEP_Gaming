e18e1f5c4c060b8547b4e96dd88b3e639601c299
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-20 23:11:32 -0800

    Copy and Paste Mike's code into BattleMessagesModel

--- Game/Game/Models/BattleMessagesModel.cs
+++ Game/Game/Models/BattleMessagesModel.cs
@@ -6,6 +6,35 @@ namespace Game.Models
 {
     public class BattleMessagesModel
     {
+        // Is the player a character or a monster
+        public PlayerTypeEnum PlayerType = PlayerTypeEnum.Unknown;
+
+        // The Status of the action
+        public HitStatusEnum HitStatus = HitStatusEnum.Unknown;
+
+        // Name of the Attacker
+        public string AttackerName = string.Empty;
+
+        // Name of who the target was
+        public string TargetName = string.Empty;
+
+        // The status of the Attack
+        public string AttackStatus = string.Empty;
+
+        // Turn Message
+        public string TurnMessage = string.Empty;
+
+        // Turn Special Message
+        public string TurnMessageSpecial = string.Empty;
+
+        // Level Up Message
+        public string LevelUpMessage = string.Empty;
+
+        // Amount of Damage
+        public int DamageAmount = 0;
+
+        // The Remaining Health Mesage
+        public int CurrentHealth = 0;
 
     }
 }
