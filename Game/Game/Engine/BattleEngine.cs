992f4e81407c899319f271d3ebeb20e7fc42ada7
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-21 10:43:06 -0800

    Add comments to BattleEngine

--- Game/Game/Engine/BattleEngine.cs
+++ Game/Game/Engine/BattleEngine.cs
@@ -5,9 +5,22 @@ using Game.Models;
 
 namespace Game.Engine
 {
+    /// <summary>
+    /// Main battle engine 
+    /// </summary>
     public class BattleEngine 
     {
+        /// <summary>
+        /// List of characters in the party
+        /// </summary>
         private List<DungeonCharacterModel> Party;
+        
+        /// <summary>
+        /// List of monsters currently fighting
+        /// </summary>
         private List<DungeonMonsterModel> Mobs;
+
+
+
     }
 }
