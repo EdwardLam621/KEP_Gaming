5c831a4bdebba895762614cc6291785f72861efc
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-21 10:52:20 -0800

    Add RoundEngine to project

---
+++ Game/Game/Engine/RoundEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Models;
+namespace Game.Engine
+{
+    public class RoundEngine
+    {
+        private int Round { get; set; } = 1;
+        private const int MAX_NUM_MONSTERS = 6;
+
+        /// <summary>
+        /// Default empty constructor
+        /// </summary>
+        public RoundEngine()
+        {
+
+        }
+
+
+
+
+        /// <summary>
+        /// Create monsters for a new round, with levels scaled to the round number
+        /// </summary>
+        /// <returns>List of new monsters</returns>
+        public List<DungeonMonsterModel> GetNewMonsters()
+        {
+            // unimplemented
+            return null;
+        }
+
+        
+    }
+}
