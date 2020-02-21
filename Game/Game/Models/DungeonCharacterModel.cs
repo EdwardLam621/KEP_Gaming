39bd60add69e094ed13d4eace321f5dfd74d866c
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-20 23:51:31 -0800

    Add DungeonCharacterModel constructors

--- Game/Game/Models/DungeonCharacterModel.cs
+++ Game/Game/Models/DungeonCharacterModel.cs
@@ -7,5 +7,40 @@ namespace Game.Models
     public class DungeonCharacterModel : CreatureModel<DungeonCharacterModel>
     {
 
+        private int Level;
+        private HashSet<ItemModel> Equipment;
+
+        /// <summary>
+        /// Empty constructor
+        /// </summary>
+        public DungeonCharacterModel()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor based on CharacterModel
+        /// </summary>
+        /// <param name="character"></param>
+        public DungeonCharacterModel(CharacterModel character)
+        {
+            ImageURI = character.ImageURI;
+            
+            Id = character.Id;
+            Name = character.Name;
+            Description = character.Description;
+            
+            Level = character.Level;
+            MaxHealth = character.MaxHealth;
+            CurrentHealth = MaxHealth;
+
+            SpeedAttribute = character.SpeedAttribute;
+            OffenseAttribute = character.OffenseAttribute;
+            DefenseAttribute = character.DefenseAttribute;
+
+            Skill = character.Skill;
+            Equipment = character.Equipments;
+
+        }
     }
 }
