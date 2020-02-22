90475fe5dc52ac7dfce39cacb34affdd53738bfd
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-21 23:25:18 -0800

    Add GetNextPlayerInList method to RoundEngine

--- Game/Game/Engine/RoundEngine.cs
+++ Game/Game/Engine/RoundEngine.cs
@@ -9,14 +9,16 @@ namespace Game.Engine
     public class RoundEngine
     {
         private int Round { get; set; } = 1;
-        
+
         private const int MAX_NUM_MONSTERS = 6;
-        
+
         private List<DungeonFighterModel> HeroList;
 
         private List<DungeonFighterModel> MonsterList;
 
         private List<DungeonFighterModel> PlayerList;
+
+        private DungeonFighterModel CurrentPlayer;
 
         /// <summary>
         /// Default empty constructor
@@ -40,12 +42,12 @@ namespace Game.Engine
         private void GetNewMonsters()
         {
 
-            MonsterList = new List<DungeonFighterModel>(); 
+            MonsterList = new List<DungeonFighterModel>();
             for (int i = 0; i < MAX_NUM_MONSTERS; i++)
             {
                 // unimplemented
             }
-           
+
         }
 
 
@@ -91,10 +93,46 @@ namespace Game.Engine
         }
         public bool StartFight()
         {
-            
-            
-            
+            //var nextPlayer = 
+
+
             return true;
+        }
+
+        /// <summary>
+        /// Get the Information about the Player
+        /// </summary>
+        /// <returns></returns>
+        public DungeonFighterModel GetNextPlayerInList()
+        {
+            // Walk the list from top to bottom
+            // If Player is found, then see if next player exist, if so return that.
+            // If not, return first player (looped)
+
+            // If List is empty, return null
+            if (PlayerList.Count == 0)
+            {
+                return null;
+            }
+
+            // No current player, so set the first one
+            if (CurrentPlayer == null)
+            {
+                return PlayerList.FirstOrDefault();
+            }
+
+            // Find current player in the list
+            var index = PlayerList.FindIndex(m => m.Id.Equals(CurrentPlayer.Id));
+
+            // If at the end of the list, return the first element
+            if (index == PlayerList.Count() - 1)
+            {
+                return PlayerList.FirstOrDefault();
+            }
+
+            // Return the next element
+            return PlayerList[index + 1];
+
         }
     }
 }
