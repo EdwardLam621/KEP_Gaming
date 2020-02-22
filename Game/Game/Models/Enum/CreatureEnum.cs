ee1a4b17ca259fe1f7bae46faf74febdb4c5868e
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-21 23:04:49 -0800

    Add CreatureEnum to project

---
+++ Game/Game/Models/Enum/CreatureEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    public enum CreatureEnum
+    {
+        Monster = 0,
+        Character = 10, 
+
+    }
+}
