587171523010d81df874ac55ea0320fc3428311c
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-22 00:10:44 -0800

    Add TurnEngine to project

---
+++ Game/Game/Engine/TurnEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Models;
+
+namespace Game.Engine
+{
+    public class TurnEngine
+    {
+        private DungeonFighterModel Attacker;
+        private DungeonFighterModel Defender;
+
+        private TurnEngine()
+        {
+
+        }
+
+        private TurnEngine(DungeonFighterModel attacker)
+        {
+            Attacker = attacker;
+        }
+
+        
+
+    }
+}
