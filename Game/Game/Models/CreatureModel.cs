49a24de21cbbed71a03adf830c048a847d98b7a9
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-22 22:55:14 -0800

    Add GetAttack, GetDefense, GetSpeed stubs to CreatureModel

--- Game/Game/Models/CreatureModel.cs
+++ Game/Game/Models/CreatureModel.cs
@@ -71,5 +71,33 @@ namespace Game.Models
 
             return myReturn.Trim();
         }
+
+        /// <summary>
+        /// Get the total attack power of the fighter
+        /// </summary>
+        /// <returns></returns>
+        public int GetAttack()
+        {
+            return 0; // unimplemented
+        }
+
+        /// <summary>
+        /// Get the total defense of the fighter
+        /// </summary>
+        /// <returns></returns>
+        public int GetDefense()
+        {
+            return 0; // unimplemented
+        }
+
+        /// <summary>
+        /// Get the total Speed of the fighter
+        /// </summary>
+        /// <returns></returns>
+        public int GetSpeed()
+        {
+            return 0; // unimplemented
+        }
+
     }
 }
