926b72b377f7ea459fd569aea7cbe6007c1d5476
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-23 22:47:58 -0800

    Add comments to Referee

--- Game/Game/Engine/Referee.cs
+++ Game/Game/Engine/Referee.cs
@@ -12,29 +12,36 @@ namespace Game.Engine
     /// </summary>
     public class Referee
     {
-
+        // List of characters in the fight
         public List<DungeonFighterModel> Characters;
 
+        // List of monsters in the current round
         public List<DungeonFighterModel> Monsters;
 
+        // Item pool
         public List<ItemModel> ItemPool = new List<ItemModel>();
 
-        // Holds the official ScoreModel
+        // Hold the official ScoreModel
         public ScoreModel BattleScore = new ScoreModel();
 
-        // Holds the Battle Messages as they happen
+        // Hold the Battle Messages as they happen
         public BattleMessagesModel BattleMessages = new BattleMessagesModel();
 
-
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Referee()
         {
 
         }
 
+        /// <summary>
+        /// Constructor taking a list of characters
+        /// </summary>
+        /// <param name="characters"></param>
         public Referee(List<DungeonFighterModel> characters)
         {
             Characters = characters;
-
         }
     }
 }
