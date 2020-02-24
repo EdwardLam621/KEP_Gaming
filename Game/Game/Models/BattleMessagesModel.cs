b2b58d30afb18985729ae92a7bf21dd1915b9699
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-23 23:16:42 -0800

    Add GetDamageMessage to BattleMessagesModel

--- Game/Game/Models/BattleMessagesModel.cs
+++ Game/Game/Models/BattleMessagesModel.cs
@@ -46,5 +46,15 @@ namespace Game.Models
             return " remaining health is " + CurrentHealth.ToString();
         }
 
+        /// <summary>
+        /// Return formatted Damage
+        /// </summary>
+        /// <returns></returns>
+        public string GetDamageMessage()
+        {
+            return string.Format(" for {0} damage ", DamageAmount);
+        }
+
+
     }
 }
