79091da31b41b6b6195cb0ae63bc7233002093b6
Edward <lamhoutenged@seattleu.edu>
2020-02-25 14:07:03 -0800

    Updated CharacterCreatePage with Level Stepper and MaxHealth change by level

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -3,6 +3,8 @@ using Game.ViewModels;
 using System;
 using Game.Models;
 using Xamarin.Forms.Xaml;
+using System.Linq;
+using Game.Helpers;
 
 namespace Game.Views
 {
@@ -21,6 +23,142 @@ namespace Game.Views
 
             this.ViewModel.Title = "Create " + data.Title;
         }
+
+
+        /// <summary>
+        /// Show the Items the Monster will drop
+        /// </summary>
+        public void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                ItemBox.Children.Remove(data);
+            }
+
+            //if not null, read all items
+            for (int i = 0; i < ViewModel.Data.DropItems.Count; i++)
+            {
+                ItemBox.Children.Add(GetItemToDisplay(ViewModel.Data.DropItems.ElementAt(i)));
+            }
+
+            //add null item as new item space
+            ItemBox.Children.Add(GetItemToDisplay(new ItemModel()));
+
+        }
+
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemModel item)
+        {
+            // Get the Item, if it exist show the info
+            // If it does not exist, show a Plus Icon for the location
+
+            // Defualt Image is the Plus
+            var ImageSource = "https://icons.iconarchive.com/icons/google/noto-emoji-smileys/1024/10024-thinking-face-icon.png";
+
+            var data = ViewModel.Data.GetDropItems(item.Id);
+            if (data == null)
+            {
+                data = new ItemModel { Name = "Add", ImageURI = ImageSource };
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            // Add a event to the user can click the item and see more
+            ItemButton.Clicked += (sender, args) => ShowPopup();
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = data.Name,
+                Style = (Style)Application.Current.Resources["ValueStyle"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = (Style)Application.Current.Resources["ItemImageBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+        }
+
+        /// <summary>
+        /// Show the Popup for the Item
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool ShowPopup()
+        {
+            PopupLoadingView.IsVisible = true;
+
+            PopupLocationLabel.Text = "Avaliable Items: ";
+
+            PopupLocationItemListView.ItemsSource = ItemIndexViewModel.Instance.GetDefaultData();
+
+
+
+            return true;
+        }
+
+        /// <summary>
+        /// When the user clicks the close in the Popup
+        /// hide the view
+        /// show the scroll view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ClosePopup_Clicked(object sender, EventArgs e)
+        {
+            ClosePopup();
+        }
+
+        /// <summary>
+        /// Close the popup
+        /// </summary>
+        private void ClosePopup()
+        {
+            PopupLoadingView.IsVisible = false;
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+
+            ItemModel data = args.SelectedItem as ItemModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            ViewModel.Data.DropItems.Add(data);
+
+            AddItemsToDisplay();
+
+            ClosePopup();
+        }
+
 
         /// <summary>
         /// Save calls to Create
@@ -56,17 +194,10 @@ namespace Game.Views
         /// <param name="e"></param>
         void Level_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         {
-            LevelValue.Text = String.Format("{0}", e.NewValue);
-        }
-
-        /// <summary>
-        /// Catch the change to the Stepper for Health
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        void Health_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            HealthValue.Text = String.Format("{0}", e.NewValue);
+            var level = e.NewValue;
+            LevelValue.Text = level.ToString();
+            ViewModel.Data.MaxHealth = DiceHelper.RollDice((int)level, 10);
+            HealthValue.Text = string.Format(" : {0:G}", ViewModel.Data.MaxHealth);
         }
 
         /// <summary>
