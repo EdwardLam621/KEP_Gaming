89a8750ac3f0ab27a444b837518aadc33a6b2f4a
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-26 13:54:10 -0800

    Add BattleGridModel to project

---
+++ Game/Game/Models/BattleGridModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    public class BattleGridModel
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BattleGridModel()
+        {
+
+        }
+
+    }
+}
