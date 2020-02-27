447ee034e04c7b7cd06a774f3932e2257eef759d
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-27 12:20:48 -0800

    Add BattleGridCellModel to project

---
+++ Game/Game/Models/BattleGridCellModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Models.Enum;
+
+namespace Game.Models
+{
+    /// <summary>
+    /// Holds information relevant to a single cell in the BattleGrid, such as players
+    /// </summary>
+    public class BattleGridCellModel
+    {
+        // Whether the cell is occupied with a character or monster, or empty
+        private BattleGridEnum CellStatus { get; set; } = BattleGridEnum.Empty;
+
+    }
+}
