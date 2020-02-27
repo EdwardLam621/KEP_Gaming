d0d484b89d6acbcca849ca70fe7546f8b9dc9a8b
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-26 21:05:11 -0800

    Add width+height constructor to BattleGridModel

--- Game/Game/Models/BattleGridModel.cs
+++ Game/Game/Models/BattleGridModel.cs
@@ -6,13 +6,23 @@ namespace Game.Models
 {
     public class BattleGridModel
     {
+
+        private int[,] MapArray; 
+
         /// <summary>
         /// Default constructor
         /// </summary>
         public BattleGridModel()
         {
-
+            
         }
+
+        public BattleGridModel(int width, int height)
+        {
+            MapArray = new int[width, height];
+        }
+
+
 
     }
 }
