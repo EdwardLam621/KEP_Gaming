382272d18733b07235ed869014e93dfd96253708
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-27 11:43:26 -0800

    Add BattleGridEnum to project

---
+++ Game/Game/Models/Enum/BattleGridEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+    public enum BattleGridEnum
+    {
+        // Empty tile
+        Empty = 0, 
+
+        // Tile holding a character
+        HasCharacter = 1, 
+        
+        // Tile holding a monster
+        HasMonster = 2,
+
+    }
+}
