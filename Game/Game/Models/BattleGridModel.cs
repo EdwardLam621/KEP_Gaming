c22672d2892c85c925c62ca70a2b2a85926b9021
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-27 12:09:07 -0800

    Update BattleGridModel fields and constructor

--- Game/Game/Models/BattleGridModel.cs
+++ Game/Game/Models/BattleGridModel.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Engine;
+using Game.Models.Enum;
 
 namespace Game.Models
 {
     public class BattleGridModel
     {
+        
+        // TileMap of pretty background images -- need to implement
+        
+        private int[,] TileMap;
+        
+        // Keep track of player and monster locations
+        private BattleGridEnum[,] PlayerGrid;
 
-        private int[,] MapArray; 
+        // Size of the board
+        private const int GRID_WIDTH = 8;
+        private const int GRID_HEIGHT = 6;
+
 
         /// <summary>
         /// Default constructor
         /// </summary>
         public BattleGridModel()
         {
-            
+            // Set up tile and player board sizes
+            TileMap = new int[GRID_WIDTH, GRID_HEIGHT];
+            PlayerGrid = new BattleGridEnum[GRID_WIDTH, GRID_HEIGHT];
         }
-
-        public BattleGridModel(int width, int height)
-        {
-            MapArray = new int[width, height];
-        }
-
-
 
     }
 }
