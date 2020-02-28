79587158a82362310fa430dd04b0b88da0331534
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-28 15:18:00 -0800

    Add Singleton to BattleEngineViewModel

--- Game/Game/ViewModels/BattleEngineViewModel.cs
+++ Game/Game/ViewModels/BattleEngineViewModel.cs
@@ -1,11 +1,38 @@
 ﻿
 
+using System;
+
 namespace Game.ViewModels
 {
  
 
     public class BattleEngineViewModel
     {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile BattleEngineViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static BattleEngineViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new BattleEngineViewModel();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
         
     }
 }
