e51e62aa716a63945f890c162f134a82321b3f74
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-29 00:29:41 -0800

    Add SkillTypeEnum to project

---
+++ Game/Game/Models/Enum/SkillTypeEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+    /// <summary>
+    /// Models which players a skill affects
+    /// </summary>
+    public enum SkillTypeEnum
+    {
+        // Unknown
+        Unknown = 0, 
+        
+        // Affect a single enemy
+        AttackOne = 1,
+
+        // Affect all enemies
+        AttackAll = 2, 
+
+        // Affect one party member
+        BuffOne = 3, 
+
+        // Affect all party members
+        BuffAll = 4,
+
+    }
+}
