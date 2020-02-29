1109ad216bdfc17aa0de6da68ca35629d532604d
Peter Weisdepp <weisdepp@seattleu.edu>
2020-02-29 00:13:29 -0800

    Add TurnChoiceEnum to project

---
+++ Game/Game/Models/Enum/TurnChoiceEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+    /// <summary>
+    /// List of choices 
+    /// </summary>
+    public enum TurnChoiceEnum
+    {
+        // Unknown
+        Unknown = 0,
+
+        // Choose monster to attack
+        Attack = 1, 
+        
+        // Choose to move around the board
+        Move = 2, 
+
+        // Choose to use a skill
+        Skill = 3,
+    }
+}
