676f287291407d460ceac323205a2500d468942a
Peter Weisdepp <weisdepp@seattleu.edu>
2020-03-02 12:21:01 -0800

    Rename method in GamePage

--- Game/Game/Views/Home/GamePage.xaml.cs
+++ Game/Game/Views/Home/GamePage.xaml.cs
@@ -26,7 +26,7 @@ namespace Game.Views
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-        async void DungeonButton_Clicked(object sender, EventArgs e)
+        async void ManualBattleButton_Clicked(object sender, EventArgs e)
         {
 			await Navigation.PushAsync(new PickCharactersPage());
 		}
