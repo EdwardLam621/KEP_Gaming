ffd7b2f830a6a11d76494d326970bc4cb9e8a3c0
Peter Weisdepp <weisdepp@seattleu.edu>
2020-03-10 02:29:33 -0700

    Refactor hackathon scenario 1 and uncomment

--- UnitTests/Engine/HackathonScenariosTests.cs
+++ UnitTests/Engine/HackathonScenariosTests.cs
@@ -1,303 +1,307 @@
-﻿//using NUnit.Framework;
+﻿using NUnit.Framework;
 
-//using Game.Engine;
-//using Game.Models;
-//using System.Threading.Tasks;
-//using Game.Helpers;
-//using System.Linq;
-//using Game.ViewModels;
+using Game.Engine;
+using Game.Models;
+using System.Threading.Tasks;
+using Game.Helpers;
+using System.Linq;
+using Game.ViewModels;
+using System.Collections.Generic;
 
-//namespace Scenario
-//{
-//    [TestFixture]
-//    public class HackathonScenarioTests
-//    {
-//        readonly BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+namespace Scenario
+{
+    [TestFixture]
+    public class HackathonScenarioTests
+    {
+        readonly BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
 
-//        AutoBattleEngine AutoBattleEngine;
-//        BattleEngine BattleEngine;
+        //AutoBattleEngine AutoBattleEngine;
+        BattleEngine BattleEngine;
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            AutoBattleEngine = EngineViewModel.AutoBattleEngine;
-//            BattleEngine = EngineViewModel.Engine;
-//        }
+        [SetUp]
+        public void Setup()
+        {
+            BattleEngine = EngineViewModel.Engine;
+        }
 
-//        [TearDown]
-//        public void TearDown()
-//        {
-//        }
+        [TearDown]
+        public void TearDown()
+        {
+        }
 
-//        [Test]
-//        public void HakathonScenario_Constructor_0_Default_Should_Pass()
-//        {
-//            /* 
-//            * Scenario Number:  
-//            *      #
-//            *      
-//            * Description: 
-//            *      <Describe the scenario>
-//            * 
-//            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
-//            *      <List Files Changed>
-//            *      <List Classes Changed>
-//            *      <List Methods Changed>
-//            * 
-//            * Test Algrorithm:
-//            *      <Step by step how to validate this change>
-//            * 
-//            * Test Conditions:
-//            *      <List the different test conditions to make>
-//            * 
-//            * Validation:
-//            *      <List how to validate this change>
-//            *  
-//            */
+        [Test]
+        public void HakathonScenario_Constructor_0_Default_Should_Pass()
+        {
+            /* 
+            * Scenario Number:  
+            *      #
+            *      
+            * Description: 
+            *      <Describe the scenario>
+            * 
+            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+            *      <List Files Changed>
+            *      <List Classes Changed>
+            *      <List Methods Changed>
+            * 
+            * Test Algrorithm:
+            *      <Step by step how to validate this change>
+            * 
+            * Test Conditions:
+            *      <List the different test conditions to make>
+            * 
+            * Validation:
+            *      <List how to validate this change>
+            *  
+            */
 
-//            // Arrange
+            // Arrange
 
-//            // Act
+            // Act
 
-//            // Assert
-           
-
-//            // Act
-//            var result = EngineViewModel;
-
-//            // Reset
-
-//            // Assert
-//            Assert.IsNotNull(result);
-//        }
-
-//        [Test]
-//        public async Task HackathonScenario_Scenario_1_Default_Should_Pass()
-//        {
-//            /* 
-//            * Scenario Number:  
-//            *      1
-//            *      
-//            * Description: 
-//            *      Make a Character called Mike, who dies in the first round
-//            * 
-//            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
-//            *      No Code changes requied 
-//            * 
-//            * Test Algrorithm:
-//            *      Create Character named Mike
-//            *      Set speed to -1 so he is really slow
-//            *      Set Max health to 1 so he is weak
-//            *      Set Current Health to 1 so he is weak
-//            *  
-//            *      Startup Battle
-//            *      Run Auto Battle
-//            * 
-//            * Test Conditions:
-//            *      Default condition is sufficient
-//            * 
-//            * Validation:
-//            *      Verify Battle Returned True
-//            *      Verify Mike is not in the Player List
-//            *      Verify Round Count is 1
-//            *  
-//            */
-
-//            //Arrange
-
-//            // Set Character Conditions
-
-//            EngineViewModel.Engine.MaxNumberPartyCharacters = 1;
-
-//            var CharacterPlayerMike = new PlayerInfoModel(
-//                            new CharacterModel
-//                            {
-//                                Speed = -1, // Will go last...
-//                                Level = 1,
-//                                CurrentHealth = 1,
-//                                ExperienceTotal = 1,
-//                                ExperienceRemaining = 1,
-//                                Name = "Mike",
-//                            });
-
-//            EngineViewModel.Engine.CharacterList.Add(CharacterPlayerMike);
-
-//            // Set Monster Conditions
-
-//            // Auto Battle will add the monsters
+            // Assert
 
 
-//            //Act
-//            var result = await AutoBattleEngine.RunAutoBattle();
+            // Act
+            var result = EngineViewModel;
 
-//            //Reset
+            // Reset
 
-//            //Assert
-//            Assert.AreEqual(true, result);
-//            Assert.AreEqual(null, AutoBattleEngine.PlayerList.Find(m => m.Name.Equals("Mike")));
-//            Assert.AreEqual(1, AutoBattleEngine.BattleScore.RoundCount);
-//        }
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
-//        [Test]
-//        public void HackathonScenario_Scenario_2_Character_Bob_Should_Miss()
-//        {
-//            /* 
-//             * Scenario Number:  
-//             *  2
-//             *  
-//             * Description: 
-//             *      Make a Character called Bob
-//             *      Bob Always Misses
-//             *      Other Characters Always Hit
-//             * 
-//             * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
-//             *      Change to Turn Engine
-//             *      Changed TurnAsAttack method
-//             *      Check for Name of Bob and return miss
-//             *                 
-//             * Test Algrorithm:
-//             *  Create Character named Bob
-//             *  Create Monster
-//             *  Call TurnAsAttack
-//             * 
-//             * Test Conditions:
-//             *  Test with Character of Named Bob
-//             *  Test with Character of any other name
-//             * 
-//             * Validation:
-//             *      Verify Enum is Miss
-//             *  
-//             */
+        [Test]
+        public async Task HackathonScenario_Scenario_1_Default_Should_Pass()
+        {
+            /* 
+            * Scenario Number:  
+            *      1
+            *      
+            * Description: 
+            *      Make a Character called Mike, who dies in the first round
+            * 
+            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+            *      No Code changes requied 
+            * 
+            * Test Algrorithm:
+            *      Create Character named Mike
+            *      Set speed to -1 so he is really slow
+            *      Set Max health to 1 so he is weak
+            *      Set Current Health to 1 so he is weak
+            *  
+            *      Startup Battle
+            *      Run Auto Battle
+            * 
+            * Test Conditions:
+            *      Default condition is sufficient
+            * 
+            * Validation:
+            *      Verify Battle Returned True
+            *      Verify Mike is not in the Player List
+            *      Verify Round Count is 1
+            *  
+            */
 
-//            //Arrange
+            //Arrange
 
-//            // Set Character Conditions
+            // Set Character Conditions
 
-//            BattleEngine.MaxNumberPartyCharacters = 1;
+            EngineViewModel.Engine.MaxNumberPartyCharacters = 1;
 
-//            var CharacterPlayer = new PlayerInfoModel(
-//                            new CharacterModel
-//                            {
-//                                Speed = 200,
-//                                Level = 10,
-//                                CurrentHealth = 100,
-//                                ExperienceTotal = 100,
-//                                ExperienceRemaining = 1,
-//                                Name = "Bob",
-//                            });
+            var CharacterPlayerMike = new CharacterModel
+                            {
+                                SpeedAttribute = -1, // Will go last...
+                                Level = 1,
+                                CurrentHealth = 1,
+                                ExperiencePoints = 1,
+                                Name = "Mike",
+                            };
 
-//            BattleEngine.CharacterList.Add(CharacterPlayer);
 
-//            // Set Monster Conditions
+            var playerList = new List<CharacterModel>();
+            playerList.Add(CharacterPlayerMike);
 
-//            // Add a monster to attack
-//            BattleEngine.MaxNumberPartyCharacters = 1;
+            EngineViewModel.Engine.SetParty(playerList);
 
-//            var MonsterPlayer = new PlayerInfoModel(
-//                new MonsterModel
-//                {
-//                    Speed = 1,
-//                    Level = 1,
-//                    CurrentHealth = 1,
-//                    ExperienceTotal = 1,
-//                    ExperienceRemaining = 1,
-//                    Name = "Monster",
-//                });
+            EngineViewModel.Engine.SetAutoBattle(true);
 
-//            BattleEngine.CharacterList.Add(MonsterPlayer);
+            // Set Monster Conditions
 
-//            // Have dice rull 19
-//            DiceHelper.EnableForcedRolls();
-//            DiceHelper.SetForcedRollValue(19);
+            // Auto Battle will add the monsters
 
-//            //Act
-//            var result = BattleEngine.TurnAsAttack(CharacterPlayer, MonsterPlayer);
 
-//            //Reset
-//            DiceHelper.DisableForcedRolls();
+            //Act
+            var result = EngineViewModel.Engine.startBattle();
 
-//            //Assert
-//            Assert.AreEqual(true, result);
-//            Assert.AreEqual(HitStatusEnum.Miss, BattleEngine.BattleMessagesModel.HitStatus);
-//        }
+            //Reset
 
-//        [Test]
-//        public void HackathonScenario_Scenario_2_Character_Not_Bob_Should_Hit()
-//        {
-//            /* 
-//             * Scenario Number:  
-//             *      2
-//             *      
-//             * Description: 
-//             *      See Default Test
-//             * 
-//             * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
-//             *      See Defualt Test
-//             *                 
-//             * Test Algrorithm:
-//             *      Create Character named Mike
-//             *      Create Monster
-//             *      Call TurnAsAttack so Mike can attack Monster
-//             * 
-//             * Test Conditions:
-//             *      Control Dice roll so natural hit
-//             *      Test with Character of not named Bob
-//             *  
-//             *  Validation
-//             *      Verify Enum is Hit
-//             *      
-//             */
+            //Assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(null, EngineViewModel.Engine.Referee.Characters.Find(m => m.Name.Equals("Mike")));
+            Assert.AreEqual(1, EngineViewModel.Engine.Referee.BattleScore.RoundCount);
+        }
 
-//            //Arrange
+        //[Test]
+        //public void HackathonScenario_Scenario_2_Character_Bob_Should_Miss()
+        //{
+        //    /* 
+        //     * Scenario Number:  
+        //     *  2
+        //     *  
+        //     * Description: 
+        //     *      Make a Character called Bob
+        //     *      Bob Always Misses
+        //     *      Other Characters Always Hit
+        //     * 
+        //     * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+        //     *      Change to Turn Engine
+        //     *      Changed TurnAsAttack method
+        //     *      Check for Name of Bob and return miss
+        //     *                 
+        //     * Test Algrorithm:
+        //     *  Create Character named Bob
+        //     *  Create Monster
+        //     *  Call TurnAsAttack
+        //     * 
+        //     * Test Conditions:
+        //     *  Test with Character of Named Bob
+        //     *  Test with Character of any other name
+        //     * 
+        //     * Validation:
+        //     *      Verify Enum is Miss
+        //     *  
+        //     */
 
-//            // Set Character Conditions
+        //    //Arrange
 
-//            BattleEngine.MaxNumberPartyCharacters = 1;
+        //    // Set Character Conditions
 
-//            var CharacterPlayer = new PlayerInfoModel(
-//                            new CharacterModel
-//                            {
-//                                Speed = 200,
-//                                Level = 10,
-//                                CurrentHealth = 100,
-//                                ExperienceTotal = 100,
-//                                ExperienceRemaining = 1,
-//                                Name = "Mike",
-//                            });
+        //    BattleEngine.MaxNumberPartyCharacters = 1;
 
-//            BattleEngine.CharacterList.Add(CharacterPlayer);
+        //    var CharacterPlayer = new PlayerInfoModel(
+        //                    new CharacterModel
+        //                    {
+        //                        Speed = 200,
+        //                        Level = 10,
+        //                        CurrentHealth = 100,
+        //                        ExperienceTotal = 100,
+        //                        ExperienceRemaining = 1,
+        //                        Name = "Bob",
+        //                    });
 
-//            // Set Monster Conditions
+        //    BattleEngine.CharacterList.Add(CharacterPlayer);
 
-//            // Add a monster to attack
-//            BattleEngine.MaxNumberPartyCharacters = 1;
+        //    // Set Monster Conditions
 
-//            var MonsterPlayer = new PlayerInfoModel(
-//                new MonsterModel
-//                {
-//                    Speed = 1,
-//                    Level = 1,
-//                    CurrentHealth = 1,
-//                    ExperienceTotal = 1,
-//                    ExperienceRemaining = 1,
-//                    Name = "Monster",
-//                });
+        //    // Add a monster to attack
+        //    BattleEngine.MaxNumberPartyCharacters = 1;
 
-//            BattleEngine.CharacterList.Add(MonsterPlayer);
+        //    var MonsterPlayer = new PlayerInfoModel(
+        //        new MonsterModel
+        //        {
+        //            Speed = 1,
+        //            Level = 1,
+        //            CurrentHealth = 1,
+        //            ExperienceTotal = 1,
+        //            ExperienceRemaining = 1,
+        //            Name = "Monster",
+        //        });
 
-//            // Have dice roll 20
-//            DiceHelper.EnableForcedRolls();
-//            DiceHelper.SetForcedRollValue(20);
+        //    BattleEngine.CharacterList.Add(MonsterPlayer);
 
-//            //Act
-//            var result = BattleEngine.TurnAsAttack(CharacterPlayer, MonsterPlayer);
+        //    // Have dice rull 19
+        //    DiceHelper.EnableForcedRolls();
+        //    DiceHelper.SetForcedRollValue(19);
 
-//            //Reset
-//            DiceHelper.DisableForcedRolls();
+        //    //Act
+        //    var result = BattleEngine.TurnAsAttack(CharacterPlayer, MonsterPlayer);
 
-//            //Assert
-//            Assert.AreEqual(true, result);
-//            Assert.AreEqual(HitStatusEnum.Hit, BattleEngine.BattleMessagesModel.HitStatus);
-//        }
-//    }
-//}
+        //    //Reset
+        //    DiceHelper.DisableForcedRolls();
+
+        //    //Assert
+        //    Assert.AreEqual(true, result);
+        //    Assert.AreEqual(HitStatusEnum.Miss, BattleEngine.BattleMessagesModel.HitStatus);
+        //}
+
+        //[Test]
+        //public void HackathonScenario_Scenario_2_Character_Not_Bob_Should_Hit()
+        //{
+        //    /* 
+        //     * Scenario Number:  
+        //     *      2
+        //     *      
+        //     * Description: 
+        //     *      See Default Test
+        //     * 
+        //     * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+        //     *      See Defualt Test
+        //     *                 
+        //     * Test Algrorithm:
+        //     *      Create Character named Mike
+        //     *      Create Monster
+        //     *      Call TurnAsAttack so Mike can attack Monster
+        //     * 
+        //     * Test Conditions:
+        //     *      Control Dice roll so natural hit
+        //     *      Test with Character of not named Bob
+        //     *  
+        //     *  Validation
+        //     *      Verify Enum is Hit
+        //     *      
+        //     */
+
+        //    //Arrange
+
+        //    // Set Character Conditions
+
+        //    BattleEngine.MaxNumberPartyCharacters = 1;
+
+        //    var CharacterPlayer = new PlayerInfoModel(
+        //                    new CharacterModel
+        //                    {
+        //                        Speed = 200,
+        //                        Level = 10,
+        //                        CurrentHealth = 100,
+        //                        ExperienceTotal = 100,
+        //                        ExperienceRemaining = 1,
+        //                        Name = "Mike",
+        //                    });
+
+        //    BattleEngine.CharacterList.Add(CharacterPlayer);
+
+        //    // Set Monster Conditions
+
+        //    // Add a monster to attack
+        //    BattleEngine.MaxNumberPartyCharacters = 1;
+
+        //    var MonsterPlayer = new PlayerInfoModel(
+        //        new MonsterModel
+        //        {
+        //            Speed = 1,
+        //            Level = 1,
+        //            CurrentHealth = 1,
+        //            ExperienceTotal = 1,
+        //            ExperienceRemaining = 1,
+        //            Name = "Monster",
+        //        });
+
+        //    BattleEngine.CharacterList.Add(MonsterPlayer);
+
+        //    // Have dice roll 20
+        //    DiceHelper.EnableForcedRolls();
+        //    DiceHelper.SetForcedRollValue(20);
+
+        //    //Act
+        //    var result = BattleEngine.TurnAsAttack(CharacterPlayer, MonsterPlayer);
+
+        //    //Reset
+        //    DiceHelper.DisableForcedRolls();
+
+        //    //Assert
+        //    Assert.AreEqual(true, result);
+        //    Assert.AreEqual(HitStatusEnum.Hit, BattleEngine.BattleMessagesModel.HitStatus);
+        //}
+    }
+}
