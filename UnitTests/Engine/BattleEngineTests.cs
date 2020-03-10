574ec476f5a7db6a524ec20c77f80d6641ffe0f8
Peter Weisdepp <weisdepp@seattleu.edu>
2020-03-10 01:06:15 -0700

    Add Battle Engine Tests from Mike

--- UnitTests/Engine/BattleEngineTests.cs
+++ UnitTests/Engine/BattleEngineTests.cs
@@ -1,10 +1,75 @@
-﻿using System;
+﻿using NUnit.Framework;
+
+using Game.Engine;
+using Game.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace UnitTests.Engine
 {
+    [TestFixture]
     public class BattleEngineTests
     {
-        public BattleEngineTests()
+        BattleEngine Engine;
+
+        [SetUp]
+        public void Setup()
         {
+            Engine = new BattleEngine();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void BattleEngine_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void BattleEngine_StartBattle_AutoModel_True_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            
+            Engine.SetAutoBattle(true);
+            var result = Engine.startBattle();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(true, Engine.Referee.BattleScore.AutoBattle);
+        }
+
+
+        [Test]
+        public void BattleEngine_PopulateCharacterList_Should_Pass()
+        {
+            // Arrange
+            var characterList = new List<CharacterModel>();
+            characterList.Add(new CharacterModel());
+            
+            // Act
+            var result = Engine.SetParty(characterList);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result);
         }
     }
 }
