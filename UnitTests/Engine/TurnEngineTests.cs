5d7fc84f233e038212b0a23e2b0503bd60df1337
Ka <khanhphan0214@gmail.com>
2020-03-19 14:12:15 -0700

    added turnEngineTests file and test for Attack() in turnEngine

---
+++ UnitTests/Engine/TurnEngineTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+using System.Linq;
+using System.Collections.Generic;
+
+using Game.Engine;
+using Game.Models;
+using Game.Helpers;
+using Game.ViewModels;
+
+namespace UnitTests.Engine
+{
+    [TestFixture]
+    public class TurnEngineTests
+    {
+        TurnEngine Engine;
+
+        [SetUp]
+        public void Setup()
+        {
+            Engine = new TurnEngine();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void TurnEngine_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void TurnEngine_Attack_Valid_Empty_Monster_List_Should_Fail()
+        {
+            // Arrange
+            var PlayerInfo = new CharacterModel();
+
+            Engine.Target = null;
+
+            // Act
+            var result = Engine.Attack(new DungeonFighterModel(PlayerInfo));
+
+            // Reset
+
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+    }
+}
